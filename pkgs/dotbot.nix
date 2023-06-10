837699de67b0c10119dc95167399d265e1d6a4ef
Sig Lange <sig.lange@gmail.com>
2023-06-09 22:28:17 -0700

    fix hash

--- pkgs/dotbot.nix
+++ pkgs/dotbot.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
     owner = "sigmonsays";
     repo = "dotbot";
     rev = "v${version}";
-    hash = "sha256-66zURNXvZllnGo803BRGpcxjny4ESmnJNiTkeHDRAmI";
+    hash = "sha256-hmQmx2AouE0Mf9s+dHl+iFEjyDuYlYNRiXqiUudn1HM";
   };
 
   vendorHash = "sha256-Zdqh8Y6zpm2aRMDtL6cNFqsHk97BOHLgVy/d2hFbguo";
