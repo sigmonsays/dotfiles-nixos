7c6ad660aa7ecfc45778e8f051349e833d58458b
Sig Lange <sig.lange@gmail.com>
2023-06-19 22:51:32 +0000

    fix

--- packages.nix
+++ packages.nix
@@ -8,7 +8,6 @@ pkgs: with pkgs; [
   # For oflist. Might be time to rewrite this thing
   #python2
 
-  ( import ./pkgs/example.nix )
   ( import ./pkgs/dotbot.nix )
   ( import ./pkgs/git-caddy.nix )
   ( import ./pkgs/findfile.nix )
