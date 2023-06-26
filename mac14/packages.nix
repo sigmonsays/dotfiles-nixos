6bef28c80c3cc7e210e34728070e7b187e9880bb
Sig Lange <sig.lange@gmail.com>
2023-06-25 22:30:55 -0700

    bits 0xbeef4c3

--- mac14/packages.nix
+++ mac14/packages.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-
     # Better userspace
     pkgs.findutils
     pkgs.coreutils
@@ -32,22 +29,9 @@
     # nix tools
     pkgs.nixos-rebuild
 
-
     # virtualization
     #pkgs.podman
     # pkgs.qemu
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 }
